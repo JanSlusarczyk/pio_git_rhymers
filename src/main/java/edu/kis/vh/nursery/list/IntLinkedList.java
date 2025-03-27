88783dec9bca173c32bd0cfe269643c97bcb50c8
janslu <252832@edu.p.lodz.pl>
2025-03-27 17:18:32 +0100

    fixed constant naming

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    private static final int defaultReturn = -1;
+    private static final int DEFAULT_RETURN = -1;
     private Node last;
     private int i;
 
@@ -27,13 +27,13 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return defaultReturn;
+            return DEFAULT_RETURN;
         return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
-            return defaultReturn;
+            return DEFAULT_RETURN;
         int ret = last.getValue();
         last = last.getPrev();
         return ret;
