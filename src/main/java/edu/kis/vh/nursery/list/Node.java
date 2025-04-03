d0e1b0653d35c8441dab7ba85a2b1a0b402c6869
janslu <252832@edu.p.lodz.pl>
2025-04-03 16:26:33 +0200

    Added docs to Node class

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,31 +1,54 @@
 package edu.kis.vh.nursery.list;
 
 public class Node {
-
     private final int value;
     private Node prev;
     private Node next;
 
+    /**
+     * Constructor for Node class
+     * @param i
+     */
     public Node(int i) {
         value = i;
     }
 
+    /**
+     * Returns value from node
+     * @return
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Returns previous node
+     * @return
+     */
     public Node getPrev() {
         return prev;
     }
 
+    /**
+     * Assigns previous node
+     * @param prev
+     */
     public void setPrev(Node prev) {
         this.prev = prev;
     }
 
+    /**
+     * Returns next node
+     * @return
+     */
     public Node getNext() {
         return next;
     }
 
+    /**
+     * Assigns next node
+     * @param next
+     */
     public void setNext(Node next) {
         this.next = next;
     }
